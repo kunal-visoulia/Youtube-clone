9f9e7b687b705d7d9b9231f27e0700e50fb68581
K_Visoulia <k_visoulia@dell.com>
2024-04-10 11:37:56 +0530

    build sidenav and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div class='flex justify-between my-2'>
+      {/* first section */}
+      <div class='flex'>
+        <img class='h-8 px-2' alt='menu'
+          src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII='
+        />
+        <img
+          className='h-8'
+          alt='youtube-logo'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png'
+        />
+      </div>
+      {/* second section */}
+      {/* gave parent width of 75% and child 50%, so they acquire more space and remain responsive at the same time */}
+      <div class='w-9/12 flex justify-center px-2'>
+        <input type='text' class='border border-gray-900 w-2/4 rounded-l-full p-2' placeholder='Search' /> {/* The p-2 gives more space inside search elemnt */}
+        <button class='border border-gray-900 rounded-r-full bg-gray-300'>🔍</button>
+      </div>
+      {/* third section */}
+      <div>
+        <img class='h-8 px-2' src='https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png' alt='user' />
+      </div>
+    </div>
   )
 }
 
--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Sidenav = () => {
   return (
-    <div>Sidenav</div>
+    <div className="p-5 shadow-lg w-48">
+      <ul>
+        <li>
+          Home
+        </li>
+        <li> Shorts</li>
+        <li> Videos</li>
+        <li> Live</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li> Music</li>
+        <li> Sports</li>
+        <li> Gaming</li>
+        <li> Movies</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li> Music</li>
+        <li> Sports</li>
+        <li> Gaming</li>
+        <li> Movies</li>
+      </ul>
+    </div>
   )
 }
 
